c740a64c2cd725bac3404061dffa025b4a35bffe
dm-larionov <112629282+dm-larionov@users.noreply.github.com>
2022-09-30 14:53:21 +0300

    init

---
+++ PrioraClass/Priora.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace PrioraClass
+{
+    public class Приора
+    {
+        public int ЛошадиныеСилы { get; }
+        public int Вес { get; }
+        public int МаксСкорость { get; }
+
+
+
+        public int ТекущаяСкорость { get; private set; } = 0;
+        public int ТекущаяПередача { get; private set; } = 0;
+        public string Цвет { get; set; } = "Белая";
+        public string Название { get; set; } = "Приора";
+        public bool РаботаетЛиДвигатель { get; private set; } = false;
+
+        public Приора(int лошадиныеСилы, int вес, int максСкорость)
+        {
+            ЛошадиныеСилы = лошадиныеСилы;
+            Вес = вес;
+            МаксСкорость = максСкорость;
+        }
+
+        public void ЗавестиМашину()
+        {
+            if (ТекущаяПередача != 0)
+            {
+                Console.WriteLine("Машина заглохла");
+                throw new Exception("Машина заглохла");
+            }
+            РаботаетЛиДвигатель = true;
+            Console.WriteLine("Машина запущена");
+        }
+
+        public void ПоменятьПередачу(int выбираемаяПередача)
+        {
+            if (выбираемаяПередача + 1 == ТекущаяПередача || выбираемаяПередача - 1 == ТекущаяПередача)
+            {
+                ТекущаяПередача = выбираемаяПередача;
+                Console.WriteLine($"Передача машины изменена, сейчас она {ТекущаяПередача}");
+                return;
+            }
+            throw new Exception("Машина заглохла");
+        }
+
+        public void ЗаглушитьМашину()
+        {
+            РаботаетЛиДвигатель = false;
+            Console.WriteLine("Машина заглушена");
+        }
+
+        public void НажатьГаз()
+        {
+            ТекущаяСкорость += 10;
+            Console.WriteLine($"Машина увеличила скорость: сейчас она {ТекущаяСкорость} км/ч");
+        }
+
+        public void Притормозить()
+        {
+            ТекущаяСкорость -= 10;
+            Console.WriteLine($"Машина уменьшила скорость: сейчас она {ТекущаяСкорость} км/ч");
+        }
+
+    }
+}
---
+++ PrioraClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrioraClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Приора москвич = new Приора(100, 5000, 60);
+
+            москвич.ЗавестиМашину();
+            москвич.ПоменятьПередачу(1);
+            москвич.НажатьГаз();
+            москвич.НажатьГаз();
+            москвич.Притормозить();
+            москвич.Притормозить();
+            москвич.ЗаглушитьМашину();
+
+            Console.ReadKey();
+        }
+    }
+}
